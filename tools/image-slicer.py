b222e56a0454979456f79b19bce44eb46cae4660
Jon Smith <llyn.glas@gmail.com>
2023-08-25 02:24:41 -0400

    Start if image slicer

--- tools/image-slicer.py
+++ tools/image-slicer.py
@@ -1,3 +1,23 @@
+import os, sys
 import PIL
+from PIL import Image
 
-print('hello')
+
+#print('hello')
+#print("CWD:", os.getcwd())
+
+cards = []
+
+try:
+    with Image.open("raw-images/BomberBoys-PNPv2-USLetter-20.png") as im:
+        new_im = im.crop((20, 20, 400, 700))
+        im.show()
+        new_im.show()
+        try:
+            new_im.save("processed-images/card1.png")
+        except (OSError, ValueError):
+            print("File not saved")
+            
+        
+except OSError:
+    print("File not opened")
